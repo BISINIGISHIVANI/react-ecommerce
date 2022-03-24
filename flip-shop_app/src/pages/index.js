771de499798f1dcd554c363d58f3bc5f36774f06
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-03-24 10:19:09 +0530

    refactor:export cart page

---
+++ flip-shop_app/src/pages/index.js
@@ -0,0 +1,6 @@
+import LandingPage from "./landing-page/Landingpage";
+import Cart from "./cart/Cart";
+import Wishlist from "./wishlist/Wishlist";
+import ProductWithFilter from "./products/Product";
+
+export { LandingPage, Cart, Wishlist, ProductWithFilter };
