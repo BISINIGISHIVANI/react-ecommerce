864f6dbf3b8a8751b5e40ee209499f1f02a4789f
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-03-26 19:23:08 +0530

    feat:add filterprovider

--- flip-shop_app/src/index.js
+++ flip-shop_app/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { makeServer } from "./server";
+import { FilterProvider } from "./hooks/context/product-context";
 
 // Call make Server
 makeServer();
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-    <App />
-    </Router>
+    <FilterProvider>
+      <Router>
+        <App />
+      </Router>
+    </FilterProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
