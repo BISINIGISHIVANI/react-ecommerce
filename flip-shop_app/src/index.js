3b94782d2a62e539160324cb100640e82bc85518
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-03-31 12:43:50 +0530

    [feat] : export wishlistprovider

--- flip-shop_app/src/index.js
+++ flip-shop_app/src/index.js
@@ -4,15 +4,18 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { makeServer } from "./server";
 import { FilterProvider } from "./hooks/context/product-context";
+import { WishlistProvider } from "./hooks/context/wishlist-context";
 
 // Call make Server
 makeServer();
 ReactDOM.render(
   <React.StrictMode>
       <Router>
+        <WishlistProvider>
         <FilterProvider>
         <App />
         </FilterProvider>
+        </WishlistProvider>
       </Router>
   </React.StrictMode>,
   document.getElementById("root")
