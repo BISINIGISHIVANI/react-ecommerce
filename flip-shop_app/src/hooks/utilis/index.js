72147acb5c72beb731fc539bdf1d476639ed47f4
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-03-31 12:40:02 +0530

    [feat] :export filter data

--- flip-shop_app/src/hooks/utilis/index.js
+++ flip-shop_app/src/hooks/utilis/index.js
@@ -1,2 +1,5 @@
-export {CategoryFilterData}from './category-filterdata';
-export { GetFilteredData, GetSortedBy }from './product-filterdata';
+export {CategoryFilterData}from './filter-handlers/category-filterdata';
+export { GetFilteredData, GetSortedBy }from './filter-handlers/product-filterdata';
+export {addToWishlistHandler} from './wishlist-handlers/addToWishlistHandler';
+export {removeFromWishlistHandler} from './wishlist-handlers/removeFromWishlistHandler';
+export {getWishlistItemsHandler} from './wishlist-handlers/getWishListItemHandler';
