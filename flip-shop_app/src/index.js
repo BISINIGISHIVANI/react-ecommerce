95988a061ea2d51fa277587e773a11d0b85f0d7b
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-04-08 22:27:38 +0530

    [feat]:export authprovider to app

--- flip-shop_app/src/index.js
+++ flip-shop_app/src/index.js
@@ -4,15 +4,18 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { makeServer } from "./server";
 import { FilterProvider } from "./hooks/context/product-context";
+import { AuthProvider } from "./hooks/context/auth-context";
 
 // Call make Server
 makeServer();
 ReactDOM.render(
   <React.StrictMode>
       <Router>
+        <AuthProvider>
         <FilterProvider>
         <App />
         </FilterProvider>
+        </AuthProvider>
       </Router>
   </React.StrictMode>,
   document.getElementById("root")
