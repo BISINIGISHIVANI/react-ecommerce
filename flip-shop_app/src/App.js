0f315b3fb440ae5016c785235b364ef213a0ebcd
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-04-09 08:14:23 +0530

    [feat]:add public and private routes

--- flip-shop_app/src/App.js
+++ flip-shop_app/src/App.js
@@ -1,21 +1,11 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { Loginpage, ForgotPage, SignupPage } from './pages/authentication/index';
-import { LandingPage, Cart, Wishlist, ProductWithFilter } from  './pages/index';
 import { Navbar, Footer } from './components/index';
+import {PublicRoute} from "./routes/public-routes/public-route"
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<Loginpage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgot-password" element={<ForgotPage />} />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/products" element={<ProductWithFilter />} />
-      </Routes>
+      <PublicRoute/>
       <Footer />
     </div>
   );
