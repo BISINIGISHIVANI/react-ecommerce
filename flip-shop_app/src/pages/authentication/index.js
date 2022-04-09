84d76ca43cb3c006277bdc9ac5973cd9d817bc94
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-04-09 08:12:59 +0530

    [refactor]:changed file paths

--- flip-shop_app/src/pages/authentication/index.js
+++ flip-shop_app/src/pages/authentication/index.js
@@ -1,4 +1,4 @@
 import Loginpage from "../authentication/login/Login";
-import ForgotPage from "../authentication/forgot-pwd/forgot";
+import ForgotPage from "../authentication/signup/forgot-pwd/forgot";
 import SignupPage from "../authentication/signup/Signup";
 export { Loginpage, ForgotPage, SignupPage };
