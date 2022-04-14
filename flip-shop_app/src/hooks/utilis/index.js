a5e1c0f6932b86be3ca1ab875e4e62b2a14ccec5
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-04-14 18:19:07 +0530

    [refactor]:change path of handlers

--- flip-shop_app/src/hooks/utilis/index.js
+++ flip-shop_app/src/hooks/utilis/index.js
@@ -1,2 +1,5 @@
 export {CategoryFilterData}from './product-utils/category-filterdata';
 export { GetFilteredData, GetSortedBy }from './product-utils/product-filterdata';
+import {removeFromWishlistHandler,addToWishlistHandler,getWishlistItemsHandler,moveToCartandler} from "./wishlist-util/wishlist-utl";
+import {addToCartHandler,getCartBill,updateCartHandler,removeFromCartHandler,moveToWishlistHandler} from "./cart-utils/cart-util";
+export {removeFromWishlistHandler,addToWishlistHandler,getWishlistItemsHandler,moveToCartandler,addToCartHandler,getCartBill,updateCartHandler,removeFromCartHandler,moveToWishlistHandler}
